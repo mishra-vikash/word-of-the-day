3b3e975af91b293bcd3fdc5abbe7111f13d459af
Vikash Chandra Mishra <Vikash.C.Mishra@aexp.com>
2020-07-18 17:38:23 -0700

    added the final version before we can go  for more animation

--- js/App.js
+++ js/App.js
@@ -1,5 +1,59 @@
 // Obtain the root
 const rootElement = document.getElementById('root')
+const words = [
+    {
+        word: "Surfeit",
+        type: "noun",
+        meaning: ["cause (someone) to desire no more of something as a result of having consumed or done it to excess."]
+    },
+    {word: "Captious", type: "adjective", meaning: ["tending to find a fault on petty Objections"]},
+    {word: "Lassitude", type: "noun", meaning: ["Lethargy, sluggishness"]},
+    {word: "Placid", type: "adjective", meaning: ["(of a person or animal) not easily upset or excited"]}
+]
+
+function Word(props) {
+
+    return <div className={"menu-container"}>
+        <div className={"ham"}>
+            <div><i className="fa fa-bars" aria-hidden="true" onClick={props.describeAction}></i></div>
+        </div>
+        <div className={"menu"}>
+            <div className="word">{props.word} ({props.type})</div>
+        </div>
+
+    </div>
+}
+
+function Meaning(props) {
+    const listItems = props.meaning.map((number) =>
+        <li>{number}</li>
+    );
+    return <div className={"menu-container"}>
+        <div className={"ham"}>
+            <div><i className="fa fa-bars" aria-hidden="true" onClick={props.describeAction}></i></div>
+        </div>
+        <div className={"menu"}>
+            <div>{props.word}</div>
+        </div>
+
+        <div className={"menu"}>
+            <ul>{listItems}</ul>
+        </div>
+    </div>
+
+}
+
+function Card(props) {
+    if (props.describe) {
+        return <div className="card">
+            <Meaning {...props}/>
+        </div>
+    } else {
+        return <div className="card">
+            <Word {...props}/>
+        </div>
+    }
+}
 
 /**
  *
@@ -10,17 +64,16 @@ const rootElement = document.getElementById('root')
 class WordOfTheDay extends React.Component {
     constructor(props) {
         super(props);
+        this.describeAction = this.describeAction.bind(this);
         this.state = {
-            current: 2, words: [
-                {word: "Surfeit", type: "noun", meaning: "cause (someone) to desire no more of something as a result of having consumed or done it to excess."},
-                {word: "Captious", type: "adjective", meaning: "tending to find a fault on petty Objections"},
-                {word: "Lassitude", type: "noun", meaning: "Lethargy, sluggishness"},
-                {word: "Placid", type: "adjective", meaning: "(of a person or animal) not easily upset or excited"}
-            ]
+            current: 0,
+            describe: false,
+            words: words
         };
     }
 
     navigate = (factor) => {
+        this.setState({describe: false})
         let current = this.state.current;
         if (this.state.current + factor > this.state.words.length - 1) {
             current = 0;
@@ -32,17 +85,30 @@ class WordOfTheDay extends React.Component {
         this.setState({current: current})
     }
 
-    render() {
-        return (<div className={"container"}>
+    describeAction = () => {
+        this.setState({describe: !this.state.describe})
+    }
 
-            <div className="card">
-                <div className="word-of-the-day">{this.state.words[this.state.current].word}</div>
-                <div className="type">{this.state.words[this.state.current].type}</div>
-                <div className="meaning">{this.state.words[this.state.current].meaning}</div>
+    render() {
+        return (<div className="container">
+            <Card word={this.state.words[this.state.current].word} type={this.state.words[this.state.current].type}
+                  meaning={this.state.words[this.state.current].meaning} describe={this.state.describe}
+                  describeAction={this.describeAction}/>
+            <div className="shift-right">
+            <div className="buttonContainer" style={{"marginBottom": "-1.5em"}}>
+                <i className="fa fa-caret-up btn" aria-hidden="true" onClick={() => this.describeAction()}
+                   style={{"fontSize": "2em"}}></i>
+            </div>
+            <div className="buttonContainer" style={{"marginBottom": "-1.5em"}}>
+                <i className="fa fa-caret-left btn" aria-hidden="true" onClick={() => this.navigate(1)}
+                   style={{"fontSize": "2.5em"}}></i>
+                <i className="fa fa-caret-right btn" aria-hidden="true" onClick={() => this.navigate(1)}
+                   style={{"fontSize": "2.5em"}}></i>
             </div>
             <div className="buttonContainer">
-                <button className={"btn btn-primary"} onClick={() => this.navigate(-1)}>Previous</button>
-                <button className={"btn btn-primary"} onClick={() => this.navigate(1)}>Next</button>
+                <i className="fa fa-caret-down btn" aria-hidden="true" onClick={() => this.describeAction()}
+                   style={{"fontSize": "2em"}}></i>
+            </div>
             </div>
         </div>)
     }
