2cf0462fe1099c28db1db3b8bd05137c82a30eef
Vikash Chandra Mishra <Vikash.C.Mishra@aexp.com>
2020-07-25 06:34:42 -0700

    added experimental swipe

--- js/Word.js
+++ js/Word.js
@@ -5,11 +5,13 @@ function Word(props) {
             <div><i className="fa fa-home" aria-hidden="true" onClick={props.home}></i></div>
         </div>
         <div className={"menu"}>
-            <div><i className="fa fa-caret-left btn left" aria-hidden="true" onClick={() => props.navigate(-1)}
-               style={{"fontSize": "2.5em"}}></i></div>
+            <div onTouchEnd={(e) => { e.preventDefault();props.navigate(-1);}}  onClick={()=> props.navigate(-1)}>
+                <i className="fa fa-caret-left btn left" aria-hidden="true" ></i>
+            </div>
             <div className="word" onClick={props.describeAction}>{props.word} ({props.type})</div>
-            <div onTouchStart={() => props.navigate(1)} ><i className="fa fa-caret-right btn right" aria-hidden="true" onClick={() => props.navigate(1)}
-               style={{"fontSize": "2.5em"}}></i></div>
+            <div onTouchEnd={(e) => {e.preventDefault();props.navigate(1)}} onClick={()=> props.navigate(1)}>
+                <i className="fa fa-caret-right btn right" aria-hidden="true" style={{"fontSize": "2.5em"}}></i>
+            </div>
         </div>
     </div>
 }
