ac4a8f319ecc636e1e72cb1e4dcc6c117063eb02
Vikash Chandra Mishra <Vikash.C.Mishra@aexp.com>
2020-08-02 23:12:44 -0700

    automated the revision word of the day

--- js/TopCard.js
+++ js/TopCard.js
@@ -1,6 +1,5 @@
 function TopCard(props) {
     let todayWord = words[words.length - 1];
-    /*
     // For future to automate
     let dayOfMonth = (new Date()).getDate();
     let dictSize = words.length - 1;
@@ -10,8 +9,8 @@ function TopCard(props) {
     }else {
         revisionId =  dictSize % dayOfMonth ;
     }
-    let revision = words[revisionId];*/
-    let rm = revisionWord.meaning.map(meaning => <p className="card-text">{meaning}</p>)
+    let revision = words[revisionId];
+    let rm = revision.meaning.map(meaning => <p className="card-text">{meaning}</p>)
     let wm = todayWord.meaning.map(meaning => <p className="card-text">{meaning}</p>)
 
     return (
@@ -27,8 +26,8 @@ function TopCard(props) {
         <div className="card" style={{width:"50vw","background" :"linear-gradient(to bottom, #f3e2c7 0%,#c19e67 50%,#b68d4c 51%,#e9d4b3 100%)"}}>
             <img src="https://as2.ftcdn.net/jpg/02/74/13/97/500_F_274139754_ZJOgZeUVLIG844NuvptJY7yJX3w1fjd5.jpg"  height={100}/>
             <div className="card-body">
-                <h5 className="card-title">{revisionWord.word}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{revisionWord.type}</h6>
+                <h5 className="card-title">{revision.word}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{revision.type}</h6>
                 <p className="card-text">{rm}</p>
             </div>
         </div>
